309fc3d6a8d1c99218060b71142e5b8f262a3e9c
donggada <koem0425@naver.com>
2021-01-22 11:41:01 +0900

    Initial commit

---
+++ src/main/java/com/example/umbrellaarea/umbrellaarea/UmbrellaAreaApplication.java
@@ -0,0 +1,13 @@
+package com.example.umbrellaarea.umbrellaarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaAreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umbrellaarea/umbrellaarea/UmbrellaAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umbrellaarea.umbrellaarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
