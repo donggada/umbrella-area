cd97a3d5dc12d3a075f4eb15229231e0f4d2c3bc
donggada <koem0425@naver.com>
2021-02-13 18:25:02 +0900

    주문페이지 정보가져오기

--- src/main/java/com/example/umbrellaarea/umbrellaarea/Controller/MainController.java
+++ src/main/java/com/example/umbrellaarea/umbrellaarea/Controller/MainController.java
@@ -1,15 +1,20 @@
 package com.example.umbrellaarea.umbrellaarea.Controller;
 
 import com.example.umbrellaarea.umbrellaarea.DTO.MemberDTO;
+import com.example.umbrellaarea.umbrellaarea.Repository.UmbrellaAreaRepository;
 import com.example.umbrellaarea.umbrellaarea.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    UmbrellaAreaRepository umbrellaAreaRepository;
 
     //메인 화면
     @RequestMapping("/")
@@ -22,8 +27,15 @@ public class MainController {
         return "map";
     }
 
-    @RequestMapping("order")
-    public String order(){
+    @RequestMapping("/order/{id}")
+    public String order(@PathVariable (name = "id") Long id, Model model){
+        if(id!=0){
+            model.addAttribute("areainfo",umbrellaAreaRepository.findById(id));
+        }else{
+            model.addAttribute("areainfo","현재위치 ");
+        }
+
+        System.out.println(model.toString());
         return "order";
     }
 }
